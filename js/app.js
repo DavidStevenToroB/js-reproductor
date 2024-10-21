1b05279621b15de2cc36f8d306ad58b9db713a9c
Sena <dimatesebas73@gmail.com>
2024-10-21 17:12:47 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+import { playList } from "./music.js";
+
+const audio = document.getElementById('audio')
+const play = document.getElementById('play')
+const pause = document.getElementById('pause')
+const forward = document.getElementById('forward')
+const rewind = document.getElementById('rewind')
+const stops = document.getElementById('stop')
+const playimg = document.getElementById('playimg')
+const nameArtist = document.getElementById('nameArtist')
+const nameSong = document.getElementById('nameSong')
+const audioSource = document.getElementById('audioSource')
+const nextSong = document.getElementById('nextSong')
+const backSong = document.getElementById('backSong')
+let count = 0
+
+    Songs(playList, count)
+
+    play.addEventListener('click', () => audio.play())
+    pause.addEventListener('click', () => audio.pause())
+    rewind.addEventListener('click', () => audio.currentTime -= 10)
+    forward.addEventListener('click', () => audio.currentTime += 10)
+    stops.addEventListener('click', () => {
+        audio.pause()
+        audio.currentTime = 0
+    })
+    backSong.addEventListener('click', () => {
+        count =  (count - 1) % playList.length
+        Songs(playList, count)
+    })
+    nextSong.addEventListener('click', () => {
+        count =  (count + 1) % playList.length
+        Songs(playList, count)
+    })
+
+ function Songs(song, count) {
+    playimg.src = song[count].img 
+    nameArtist.textContent = song[count].artist
+    nameSong.textContent = song[count].title
+    audioSource.src = song[count].song
+
+    audio.load()
+    audio.play()
+ }
+
+
+
+
+
+
---
+++ js/music.js
@@ -0,0 +1,32 @@
+export const playList = [
+    {
+      title: 'LOS HABITANTES',
+      artist: 'bunbury',
+      img: './assents/img/habitantes.png',
+      song: './assents/music/loshabitantes.mp3'
+    },
+    {
+      title: 'APUESTA POR EL R´N´R',
+      artist: 'bunbury',
+      img: './assents/img/apuesta-aun-mayor.avif',
+      song: './assents/music/rnr.mp3'
+    },
+    {
+      title: 'AL FINAL',
+      artist: 'bunbury',
+      img: './assents/img/alfinal.png',
+      song: './assents/music/alFinal.mp3'
+    },
+    {
+      title: 'DE MAYOR',
+      artist: 'bunbury',
+      img: './assents/img/apuesta-aun-mayor.avif',
+      song: './assents/music/demayor.mp3'
+    },
+    {
+      title: 'AUNQUE NO SEA CONMIGO',
+      artist: 'bunbury',
+      img: './assents/img/apuesta-aun-mayor.avif',
+      song: './assents/music/aunquenoseaconmigo.mp3'
+    }
+  ]
